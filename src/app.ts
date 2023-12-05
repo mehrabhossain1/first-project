ff93f17c8e015548178c75a50226892a3aad4c27
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-05 19:26:08 +0600

    module-11

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import { StudentRoutes } from './app/modules/student/student.route'
-import { UserRoutes } from './app/modules/user/user.route'
 import globalErrorHandler from './app/middlewares/globalErrorhandler'
 import notFound from './app/middlewares/notFound'
+import router from './app/routes'
 
 const app: Application = express()
 
@@ -15,8 +15,7 @@ app.use(express.json())
 app.use(cors())
 
 //application routes
-app.use('/api/v1/students', StudentRoutes)
-app.use('/api/v1/users', UserRoutes)
+app.use('/api/v1', router)
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10
