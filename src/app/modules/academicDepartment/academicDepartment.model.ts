cbe5d6fb9dce4a9ef3d20fce97a217c2c731ad19
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-10 18:25:20 +0600

    Created AppError

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from 'mongoose'
 import { TAcademicDepartment } from './academicDepartment.interface'
+import AppError from '../../errors/AppError'
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   {
@@ -32,11 +33,10 @@ academicDepartmentSchema.pre('save', async function (next) {
 
 academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const query = this.getQuery()
-
   const isDepartmentExist = await AcademicDepartment.findOne(query)
 
   if (!isDepartmentExist) {
-    throw new Error('This department does not exist!')
+    throw new AppError(404, 'This department does not exist!')
   }
 
   next()
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -4,15 +4,27 @@ import { Student } from './student.model'
 const getAllStudentsFromDb = async () => {
   const result = await Student.find()
     .populate('admissionSemester')
-    .populate('academicDepartment')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    })
 
   return result
 }
 
 const getSingleStudentFromDb = async (id: string) => {
-  // const result = await Student.findOne({ id })
+  const result = await Student.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    })
 
-  const result = await Student.aggregate([{ $match: { id: id } }])
+  // const result = await Student.aggregate([{ $match: { id: id } }])
 
   return result
 }
