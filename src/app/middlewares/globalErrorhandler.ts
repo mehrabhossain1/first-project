d5b538343f93151b46280349c3cea9f039fa9e4c
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-12 18:14:12 +0600

    Created handleCastError

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -7,6 +7,7 @@ import config from '../config'
 import { TErrorSources } from '../interface/error'
 import handleZodError from '../errors/handleZodError'
 import handleValidationError from '../errors/handleValidationError'
+import handleCastError from '../errors/handleCastError'
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
@@ -27,6 +28,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorSources = simplifiedError?.errorSources
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err)
     statusCode = simplifiedError?.statusCode
     message = simplifiedError?.message
     errorSources = simplifiedError?.errorSources
