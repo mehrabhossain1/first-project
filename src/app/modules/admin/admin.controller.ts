54411bc2a7fb59af0422b7267c6537d276bfa458
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-16 13:05:39 +0600

    admin all routes done

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -14,6 +14,46 @@ const getAllAdmins = catchAsync(async (req, res) => {
   })
 })
 
+const getSingleAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params
+  const result = await AdminServices.getSingleAdminFromDB(id)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin is retrieved successfully',
+    data: result,
+  })
+})
+
+const updateAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params
+  const { admin } = req.body
+  const result = await AdminServices.updateAdminIntoDB(id, admin)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin is updated successfully',
+    data: result,
+  })
+})
+
+const deleteAdmin = catchAsync(async (req, res) => {
+  const { id } = req.params
+  const result = await AdminServices.deleteAdminFromDB(id)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin is deleted successfully',
+    data: result,
+  })
+})
+
 export const AdminControllers = {
   getAllAdmins,
+  getSingleAdmin,
+  updateAdmin,
+  deleteAdmin,
 }
