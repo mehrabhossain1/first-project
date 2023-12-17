388ce14ef9effce1f1e4f7288070472378c81036
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-17 15:35:56 +0600

    course routes done

---
+++ src/app/modules/course/course.route.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import validateRequest from '../../middlewares/validateRequest'
+import { CourseValidations } from './course.validation'
+import { CourseControllers } from './course.controller'
+
+const router = express.Router()
+
+router.post(
+  '/create-course',
+  validateRequest(CourseValidations.crateCourseValidationSchema),
+  CourseControllers.createCourse,
+)
+
+router.get('/', CourseControllers.getAllCourse)
+
+router.get('/:id', CourseControllers.getSingleCourse)
+
+// router.patch(
+//   '/:facultyId',
+//   validateRequest(
+//     AcademicFacultyValidations.updateAcademicFacultyValidationSchema,
+//   ),
+//   AcademicFacultyControllers.updateAcademicFaculty,
+// )
+
+router.delete('/:id', CourseControllers.deleteCourse)
+
+export const CourseRoutes = router
